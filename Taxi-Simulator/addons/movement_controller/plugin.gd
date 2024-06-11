a1d7d9b02edff7b120b5aa6052de8ea3ece39d42
Soromytko <NikitaSoromytko@gmail.com>
2024-06-11 03:53:02 +0300

    Add the Movement Controller plugin

---
+++ Taxi-Simulator/addons/movement_controller/plugin.gd
@@ -0,0 +1,14 @@
+tool
+extends EditorPlugin
+#
+#const class_movement_model : Script = preload("./scripts/movement_model.gd")
+#const plugin_name := "Movement Controller"
+#const movement_model_type_name := "MovementModel (%s plugin)" % plugin_name
+#
+#
+#func _enter_tree():
+#	add_custom_type(movement_model_type_name, "Resource", class_movement_model, null)
+#
+#
+#func _exit_tree():
+#	remove_custom_type(movement_model_type_name)
---
+++ Taxi-Simulator/addons/movement_controller/scripts/movement_controller.gd
@@ -0,0 +1,62 @@
+extends Spatial
+
+export(NodePath) var kinematic_body_node_path
+
+var kinematic_body : KinematicBody setget set_kinematic_body, get_kinematic_body
+
+var velocity : Vector3 = Vector3.ZERO
+
+
+func _ready():
+	kinematic_body = get_node(kinematic_body_node_path)
+
+
+func set_kinematic_body(value):
+	kinematic_body = value
+
+
+func get_kinematic_body() -> KinematicBody:
+	return kinematic_body
+
+
+func is_grounded() -> bool:
+	return kinematic_body.is_on_floor()
+
+
+func accelerate_horizontal(direction : Vector3, acceleration : float, max_speed : float):
+	var horizontal_velocity := _get_horizontal_velocity()
+	if horizontal_velocity.length() < max_speed || true:
+		horizontal_velocity += direction * acceleration
+		if horizontal_velocity.length() > max_speed:
+			horizontal_velocity = horizontal_velocity.normalized() * max_speed
+	_set_horizontal_velocity(horizontal_velocity)
+
+
+func decelerate_horizontal(decceleration : float):
+	var horizontal_velocity := _get_horizontal_velocity()
+	horizontal_velocity = horizontal_velocity.move_toward(Vector3.ZERO, decceleration)
+	_set_horizontal_velocity(horizontal_velocity)
+
+
+func accelerate_vertical(acceleration : float, max_speed : float):
+	velocity.y = min(velocity.y + acceleration, max_speed) if acceleration > 0 \
+				else max(velocity.y + acceleration, -max_speed)
+
+
+func apply_velocity():
+	velocity = kinematic_body.move_and_slide(velocity, Vector3.UP)
+
+
+func fade_horizontal_velocity(damping : float, max_speed : float):
+	var horizontal_velocity := _get_horizontal_velocity()
+	if horizontal_velocity.length() > max_speed:
+		horizontal_velocity = horizontal_velocity.move_toward(horizontal_velocity.normalized() * max_speed, damping)
+	_set_horizontal_velocity(horizontal_velocity)
+
+
+func _get_horizontal_velocity() -> Vector3:
+	return Vector3(velocity.x, 0, velocity.z)
+
+
+func _set_horizontal_velocity(value : Vector3):
+	velocity = Vector3(value.x, velocity.y, value.z)
