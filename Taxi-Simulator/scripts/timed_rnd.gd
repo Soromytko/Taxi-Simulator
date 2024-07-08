b8b940b39835f7876284d0fdc98e5ba0b94723f0
Soromytko <NikitaSoromytko@gmail.com>
2024-07-08 10:05:22 +0300

    Make the TimedRNG more random

--- Taxi-Simulator/scripts/timed_rnd.gd
+++ Taxi-Simulator/scripts/timed_rnd.gd
@@ -3,5 +3,5 @@ class_name TimedRNG
 
 
 func _init():
-	var hashed := Time.get_time_dict_from_system().hash()
+	var hashed := Time.get_time_dict_from_system().hash() + hash(self.to_string())
 	set_seed(hashed)
