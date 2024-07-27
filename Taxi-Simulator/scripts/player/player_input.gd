c40848ca7bb93e24b0a6c3f4c56b8e45265d8008
Soromytko <NikitaSoromytko@gmail.com>
2024-07-27 23:32:28 +0300

    Add car driving to the player

--- Taxi-Simulator/scripts/player/player_input.gd
+++ Taxi-Simulator/scripts/player/player_input.gd
@@ -8,9 +8,23 @@ static func get_move_axes() -> Vector3:
 	return input.normalized() if input.length() > 1 else input
 
 
+static func get_steering_axis() -> float:
+	var left : float = Input.get_action_strength("steering_left")
+	var right : float = Input.get_action_strength("steering_right")
+	return right - left
+
+
 static func get_is_jump() -> bool:
 	return Input.get_action_strength("jump") == 1
 
 
 static func get_is_action() -> bool:
 	return Input.is_action_just_pressed("action")
+
+
+static func get_is_car_forward() -> bool:
+	return Input.is_action_pressed("car_forward")
+
+
+static func get_is_car_back() -> bool:
+	return Input.is_action_pressed("car_back")
--- Taxi-Simulator/scripts/player/states/drive_state.gd
+++ Taxi-Simulator/scripts/player/states/drive_state.gd
@@ -3,6 +3,8 @@ extends "./player_state.gd"
 export var _player_node_path : NodePath
 var vehicle : Vehicle
 onready var _player : RiderCharacter = get_node(_player_node_path)
+var _steering : float = 0.0
+var _steering_speed : float = 5.0
 
 
 func _on_enter():
@@ -12,4 +14,18 @@ func _on_enter():
 func _on_physics_update(delta : float):
 	if PlayerInput.get_is_action():
 		HumanVehicleInteraction.get_out_of_vehicle_instantly(vehicle, _player)
+		return
+	
+	if PlayerInput.get_is_car_forward():
+		vehicle.press_gas(25)
+	else:
+		vehicle.depress_gas()
 		
+	if PlayerInput.get_is_car_back():
+		vehicle.press_brake(5)
+	else:
+		vehicle.depress_brake()
+	
+	var steering_target : float = PlayerInput.get_steering_axis()
+	_steering = move_toward(_steering, steering_target, delta * _steering_speed)
+	vehicle.set_steering_progress(-_steering)
